f708340d0bcba1d1867aadb7de98bcec46cd914a
Al Masum Fahim <masum_fahim@hotmail.com>
2018-02-14 13:53:43 +0600

    Registration System Implemented

--- Angular_Frontend/src/app/views/pages/register.component.ts
+++ Angular_Frontend/src/app/views/pages/register.component.ts
@@ -1,13 +1,33 @@
+import { Router } from '@angular/router';
+import { UserService } from './../../../services/user.service';
 import { Component } from '@angular/core';
 
 @Component({
   templateUrl: 'register.component.html'
 })
 export class RegisterComponent {
-
-  constructor() { }
+  private loginError = false;
+  private loginErrorText;
+  constructor(private _userService: UserService, private router: Router) { }
 
   register(r) {
     console.log(JSON.stringify(r.value));
+    this._userService.register(r.value).subscribe(
+      res => {
+        localStorage.setItem('token', res['token']);
+        this.router.navigate(['dashboard']);
+      },
+      (error: Response) => {
+        this.loginError = true;
+        if (error.status === 400) {
+          this.loginErrorText = 'Email Format or Password is not correct';
+        }
+        if (error.status === 401) {
+          this.loginErrorText = 'Password is not correct';
+        }
+        if (error.status === 404) {
+          this.loginErrorText = 'User does not exist';
+        }
+      });
   }
 }
